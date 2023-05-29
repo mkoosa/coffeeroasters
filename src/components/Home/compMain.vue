9d4ea2300cd14c0eabe6ffe50311277c7b2866ee
Marcin Kosecki <marcin.kosecki@int.pl>
2023-05-29 20:07:05 +0200

    Create compMain.vue

---
+++ src/components/Home/compMain.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="main">
+    <comp-introduction></comp-introduction>
+    </div>
+    
+</template>
+
+<script>
+import compIntroduction from './compIntroduction.vue';
+
+export default {
+
+components: {compIntroduction}
+
+
+
+}
+
+
+
+</script>
+
+<style scoped>
+.main{
+    margin: 3rem 0;
+}
+
+
+</style>
+
