4fc26e0c501baf00704cb32544c5ed817a1fa3df
Marcin Kosecki <marcin.kosecki@int.pl>
2023-05-30 20:39:29 +0200

    Install firebase

--- src/main.js
+++ src/main.js
@@ -1,20 +1,30 @@
 import { createApp } from 'vue'
 import App from './App.vue';
 
+
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
+import Store from './Store/index';
+
 
 // components
-import MainButton from './Utils/buttons/compButtonMain.vue'
+import MainButton from './Utils/buttons/compButtonMain.vue';
+
 
 
 library.add(fas, far, fab)
 dom.watch();
 const app = createApp(App);
-app.component("font-awesome-icon", FontAwesomeIcon)
-app.component('btn-main', MainButton)
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.component('btn-main', MainButton);
+app.use(Store);
+app.mount('#app');
 
-app.mount('#app')
+
+
+
+
+
