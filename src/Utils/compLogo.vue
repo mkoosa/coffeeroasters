84fdfd2856a6c0ddc6999b02f2b19f9ec34d813a
Marcin Kosecki <marcin.kosecki@int.pl>
2023-06-01 14:05:00 +0200

    Create and add compLogo.vue

---
+++ src/Utils/compLogo.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="logo__wrapper">
+        <div class="logo">
+            <img src="../assets/home/desktop/icon-coffee-bean.svg" alt="coffee bean" class="bean bean--first">
+            <img src="../assets/home/desktop/icon-coffee-bean.svg" alt="coffee bean" width="20px" class="bean bean--second">
+        </div>
+        <h1 class="heading" :class="$attrs.class">coffeeroasters</h1>
+    </div>
+</template>
+
+
+<script>
+export default {
+
+}
+
+</script>
+
+<style scoped>
+.logo__wrapper {
+    display: flex;
+    align-items: center;
+}
+
+.logo {
+    align-self: end;
+    position: relative;
+}
+
+.bean--first {
+    position: absolute;
+    bottom: 1.2rem;
+    right: .8rem;
+    z-index: 1;
+    transform: rotate(-10deg);
+    width: 1.6rem;
+}
+
+.bean--second {
+    transform: rotate(100deg);
+    z-index: 20;
+    width: 1.6rem;
+}
+
+.heading {
+    margin-left: .5rem;
+    font-size: 2.2rem;
+    font-family: 'Fraunces', serif;
+    font-weight: 900;
+    color: var(-dark-grey-blue);
+    opacity: .9;
+}
+
+.bigger{
+    font-size: 2.8rem;
+    margin-left: 1rem;
+}
+</style>
--- src/components/Header/compHeader.vue
+++ src/components/Header/compHeader.vue
@@ -1,31 +1,22 @@
 <template>
     <div class="header">
-        <div class="header__logo">
-            <div class="logo">
-                <img src="../../assets/home/desktop/icon-coffee-bean.svg" alt="coffee bean" width="16px"
-                    class="bean bean--first">
-                <img src="../../assets/home/desktop/icon-coffee-bean.svg" alt="coffee bean" width="16px"
-                    class="bean bean--second">
-            </div>
-            <h1 class="heading">coffeeroasters</h1>
-        </div>
+        <comp-logo></comp-logo>
         <comp-nav></comp-nav>
-
     </div>
 </template>
 
 <script>
-import compNav from './compNav.vue'
+import compNav from './compNav.vue';
+import compLogo from '@/Utils/compLogo.vue';
 export default {
-    components: { compNav }
+    components: { compNav, compLogo }
 
 }
 
 </script>
 
 <style>
-
-.header{
+.header {
     display: flex;
     align-items: center;
     justify-content: space-between;
@@ -34,35 +25,5 @@ export default {
 
 .header__logo {
     display: flex;
-}
-
-.logo {
-    margin-left: 1rem;
-    align-self: end;
-    position: relative;
-}
-
-.bean--first {
-    position: absolute;
-    bottom: 1.2rem;
-    right: .8rem;
-    z-index: 1;
-    transform: rotate(-10deg);
-}
-
-.bean--second {
-    transform: rotate(100deg);
-    z-index: 20;
-}
-
-
-
-.heading {
-    margin-left: .5rem;
-    font-size: 2.2rem;
-    font-family: 'Fraunces', serif;
-    font-weight: 900;
-    color: var(-dark-grey-blue);
-    opacity: .9;
 }
 </style>
