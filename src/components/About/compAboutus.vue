94153ee25872bb5e705ebe52e21136d870a3a2f1
Marcin Kosecki <marcin.kosecki@int.pl>
2023-06-06 10:25:06 +0200

    Create compAboutus.vue

---
+++ src/components/About/compAboutus.vue
@@ -0,0 +1,28 @@
+<template>
+    <section class="about-us">
+        <comp-introductory></comp-introductory>
+        <comp-details></comp-details>
+    </section>
+</template>
+
+<script>
+import compIntroductory from './compIntroductory.vue';
+import compDetails from './compDetails.vue';
+
+export default {
+    components: { compIntroductory, compDetails },
+
+
+
+ }   
+
+
+</script>
+
+<style scoped>
+.about-us{
+    margin-top: 6rem;
+
+}
+
+</style>
