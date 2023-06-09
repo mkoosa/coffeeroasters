6a1136d293feca525c1c23c3292d02a0b5836cfb
Marcin Kosecki <marcin.kosecki@int.pl>
2023-06-09 15:21:51 +0200

    Create compPlan.vue

---
+++ src/components/Plan/compPlan.vue
@@ -0,0 +1,40 @@
+
+<template>
+    <section class="plan">
+        <compDetail :detail="detail" :isPlan="isPlan" />
+        <comp-instruction></comp-instruction>
+    </section>
+</template>
+<script>
+import compDetail from '../About/compDetail..vue';
+import compInstruction from '../Home/compInstruction.vue';
+
+export default {
+    data() {
+        return {
+            isPlan: true
+        }
+    },
+
+    components: { compDetail, compInstruction },
+    computed: {
+        detail() {
+            return this.$store.getters['plan/getDetails']
+        }
+    },
+
+    mounted() {
+        this.$store.dispatch('plan/getDetails')
+    }
+
+
+}
+
+</script>
+
+<style scoped>
+.plan {
+    margin-top: 6rem;
+
+}
+</style>
