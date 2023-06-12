b866e35685e374cb8476437bcfbece0e9e840cb0
Marcin Kosecki <marcin.kosecki@int.pl>
2023-06-12 20:24:45 +0200

    Implement compButtonMain.vue

--- src/Utils/buttons/compButtonMain.vue
+++ src/Utils/buttons/compButtonMain.vue
@@ -1,10 +1,23 @@
 <template>
-    <button class="main-btn" v-text="text" tabindex="0"></button>
+    <router-link v-if="!isDisabled" :to="{ name: 'create your plane' }">
+        <button v-if="isPlan" :class="['main-btn']" v-text="btnTxt" tabindex="0" :disabled="isDisabled"></button>
+        <button v-else :class="['main-btn']" v-text="text" tabindex="0" :disabled="isDisabled"></button>
+    </router-link>
+
+    <router-link v-else :to="{ name: 'create your plane' }" :style="{
+        pointerEvents: 'none',
+    }">
+        <button v-if="isPlan" :class="['main-btn', { 'btn--grey': isPlan }]" v-text="btnTxt" tabindex="0"
+            :disabled="isDisabled"></button>
+        <button v-else :class="['main-btn', { 'btn--grey': isPlan }]" v-text="text" tabindex="0"
+            :disabled="isDisabled"></button>
+    </router-link>
 </template>
 
 <script>
 export default {
     props: ['text'],
+    inject: ['isDisabled', 'isPlan', 'btnTxt']
 }
 
 </script>
@@ -22,13 +35,17 @@ export default {
     letter-spacing: .1rem;
     cursor: pointer;
     position: relative;
-    
+    transition: opacity .1s;
 }
 
 .main-btn:hover {
-    
-    opacity: .7;
-    background-color: var(--dark-cyan-hover);
+    opacity: .9;
+    color: var(--light-grey);
+}
+
+.btn--grey {
+    background-color: var(--light-grey);
+    pointer-events: none;
 }
 </style>
 
